366bf8ee511011e090c7d1c0fd80da6a2cfaf379
Claudio Bley <claudio.bley@gmail.com>
2024-03-12 12:22:01 +0100

    DBG: change name of tests so it is clear if hacks are enabled

--- pkgs/development/tools/build-managers/bazel/cpp-test.nix
+++ pkgs/development/tools/build-managers/bazel/cpp-test.nix
@@ -40,7 +40,7 @@ let
   ''));
 
   testBazel = bazelTest {
-    name = "bazel-test-cpp";
+    name = "${bazel.pname}-test-cpp";
     inherit workspaceDir;
     bazelPkg = bazel;
     bazelScript = ''
